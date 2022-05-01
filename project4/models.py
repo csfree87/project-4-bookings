fbb8f195660d0e4f42f2267f6467b9266f7d6579
csfree87 <csfree@icloud.com>
2022-05-01 22:46:38 +0000

    Added booking.html and views/models for reversation system

--- project4/models.py
+++ project4/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    opening_time = models.IntegerField()
+    closing_time = models.IntegerField()
+
+
+class Table(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    size = models.IntegerField()
+
+
+class Booking(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    people = models.IntegerField()
+    booking_date_time_start = models.DateTimeField()
+    booking_date_time_end = models.DateTimeField()
--- project4/views.py
+++ project4/views.py
@@ -7,3 +7,7 @@ def index(request):
 
 def menu(request):
     return render(request, 'menu.html')
+
+def booking(request):
+    return render(request, 'booking.html')
+
